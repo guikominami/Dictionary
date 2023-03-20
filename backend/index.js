6b6cb30ee0b58d10ee306f9402e042d5bfcd6cd9
guikominami <guilherme_kominami@hotmail.com>
2023-03-20 21:26:23 +0000

    atualizado erro conexao perdida

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,52 @@ import cors from "cors"
 
 const app = express()
 
-const db = mysql.createConnection({
+var db_config = {
   host: "bd_comunica.mysql.dbaas.com.br",
   user: "bd_comunica",
   password: "C0munic@1503",
   database: "bd_comunica"
-})
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.use(express.json())
 app.use(cors())
 
 app.get("/", (req, res)=>{
-  res.json("hello, this is backend!")
+  try{
+    res.json("hello, this is backend!")
+  }catch(err){
+    console.log(err);
+  }  
 })
 
 app.get("/languages", (req, res)=>{
   const querySelect = "SELECT * FROM all_languages"
-  db.query(querySelect, (err, data) => {
+  connection.query(querySelect, (err, data) => {
     if (err) return res.json(err)
       return res.json(data);
   })
@@ -32,7 +61,7 @@ app.post("/languages", (req, res)=>{
       req.body.title,
       req.body.location
     ];
-    db.query(queryInsert, [values], (err, data) => {
+    connection.query(queryInsert, [values], (err, data) => {
       if (err) return res.json(err)
       return res.json("Language has been created!")
     })
@@ -40,5 +69,9 @@ app.post("/languages", (req, res)=>{
 
 
 app.listen(8800, ()=> {
-  console.log('Connected to backend!')
+  try{
+    console.log('Connected to backend!')
+  }catch(err){
+    console.log(err);
+  }  
 })
